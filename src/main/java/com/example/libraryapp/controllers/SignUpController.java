8d3ef54c2611c11b09c4b28265b927cd74e60b80
rishchuk <i.roman.zp@gmail.com>
2021-11-27 17:36:44 +0100

    update

--- src/main/java/com/example/libraryapp/controllers/SignUpController.java
+++ src/main/java/com/example/libraryapp/controllers/SignUpController.java
@@ -1,11 +1,17 @@
 package com.example.libraryapp.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class SignUpController {
 
@@ -31,8 +37,42 @@ public class SignUpController {
     private TextField confirmSignUpPassField;
 
     @FXML
-    void initialize() {
+    private Button cancelButton;
 
+    @FXML
+    private Label createErrorLabel;
+
+    @FXML
+    void initialize() {
+        cancelButton.setOnAction(actionEvent -> {
+            cancelButton.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader(SignUpController.class.getResource("/com/example/libraryapp/hello-view.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+
+        signUpButtonCreate.setOnAction(actionEvent -> {
+            try {
+                checkCreate();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void checkCreate() throws IOException {
+        if (signUpLoginField.getText().toString() == "" || signUpPassField.getText().toString() == "" || signUpUsername.getText().toString() == "" || confirmSignUpPassField.getText().toString() == "") {
+            createErrorLabel.setText("Fill in all the fields");
+        } else if (signUpPassField.getText().toString() != confirmSignUpPassField.getText().toString()) {
+            createErrorLabel.setText("Password mismatch");
+        }
     }
 }
-
