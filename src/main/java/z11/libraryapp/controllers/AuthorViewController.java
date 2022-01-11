8bb88f4cdb74187f2ae7ce9f223031af6cd07bf1
Mikalai Stelmakh <01165876@pw.edu.pl>
2022-01-11 15:03:08 +0100

    Add author view.

--- src/main/java/z11/libraryapp/controllers/AuthorViewController.java
+++ src/main/java/z11/libraryapp/controllers/AuthorViewController.java
@@ -5,14 +5,21 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import z11.libraryapp.DbHandler;
+import z11.libraryapp.errors.UnavailableDB;
 import z11.libraryapp.model.Author;
+import z11.libraryapp.model.Book;
 import z11.libraryapp.model.User;
+import java.util.ArrayList;
+
 
 public class AuthorViewController {
 
@@ -34,10 +41,13 @@ public class AuthorViewController {
     private Label authorName1;
 
     @FXML
+    private Label authorBiography;
+
+    @FXML
     private ImageView authorImage;
 
     @FXML
-    private GridPane booksByAuthorContainer;
+    private GridPane authorBooks;
 
     @FXML
     private Button authorsButton;
@@ -95,6 +105,14 @@ public class AuthorViewController {
         authorName.setText(author.getName());
         authorName1.setText(author.getName());
         authorBirthYear.setText(Integer.toString(author.getBirthYear()));
+        if (author.getbiography().equals("")){
+            authorBiography.setText("No biogaphy available.");
+        }
+        else{
+            authorBiography.setText(author.getbiography());
+            authorBiography.setWrapText(true);
+        }
+
         int author_death_year = author.getDeathYear();
         if (author_death_year == 0){
             authorDeathYear.setText("now");
@@ -104,6 +122,30 @@ public class AuthorViewController {
         }
         Image image = new Image(getClass().getResourceAsStream("/z11/libraryapp/img/photos/" + author.getPhotoSrc()));
         authorImage.setImage(image);
+
+        try {
+            DbHandler dbManager = new DbHandler();
+            ArrayList<Book> books = dbManager.getAuthorBooks(author.getId());
+            int col = 1;
+            int row = 1;
+            for(Book book : books){
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(BookController.class.getResource("/z11/libraryapp/fxml/Book.fxml"));
+                VBox bookBox = fxmlLoader.load();
+                BookController bookController = fxmlLoader.getController();
+                bookController.setData(book, user_object);
+
+
+                if (col==5){
+                    col = 1;
+                    ++row;
+                }
+                authorBooks.add(bookBox, col++, row);
+                GridPane.setMargin(bookBox, new Insets(10));
+            }
+        } catch (UnavailableDB | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
