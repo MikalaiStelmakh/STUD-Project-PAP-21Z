6dcae9e5ab44b0e6b9e6f505db97dac3efa2c0de
Mikalai Stelmakh <01165876@pw.edu.pl>
2022-01-12 23:10:24 +0100

    Add 'reading' page.

--- src/main/java/z11/libraryapp/controllers/ReadingController.java
+++ src/main/java/z11/libraryapp/controllers/ReadingController.java
@@ -1,6 +1,7 @@
 package z11.libraryapp.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,8 +13,15 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import z11.libraryapp.DbHandler;
 import z11.libraryapp.errors.UnavailableDB;
+import z11.libraryapp.model.Book;
+import z11.libraryapp.model.BookInstance;
+import z11.libraryapp.model.HistoryNode;
 import z11.libraryapp.model.User;
 
 public class ReadingController {
@@ -27,6 +35,9 @@ public class ReadingController {
     private TextField searchField;
 
     @FXML
+    private GridPane booksContainer;
+
+    @FXML
     private Button authorsButton;
 
     @FXML
@@ -34,9 +45,6 @@ public class ReadingController {
 
     @FXML
     private Button dashboardButton;
-
-    @FXML
-    private Button historyButton;
 
     @FXML
     private Button readingButton;
@@ -63,19 +71,91 @@ public class ReadingController {
     }
 
     @FXML
-    void historyButtonOnAction(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = MainWindowController.changeScene(event, "/z11/libraryapp/fxml/History.fxml");
-        HistoryController controller = fxmlLoader.getController();
-        controller.setData(user_object);
+    void readingButtonOnAction(ActionEvent event) throws IOException {
     }
 
-    @FXML
-    void readingButtonOnAction(ActionEvent event) throws IOException {
+    private void setReadingBooks(ArrayList<HistoryNode> historyNodes){
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(AuthorController.class.getResource("/z11/libraryapp/fxml/UserBooks.fxml"));
+            VBox userBooksBox = fxmlLoader.load();
+            UserBooksController userBooksController = fxmlLoader.getController();
+            userBooksController.setData(historyNodes, user_object, "Reading", false);
+            booksContainer.add(userBooksBox, 0, 1);
+            Line line = new Line(-100, 0, 1105, 0);
+            line.setOpacity(0.12);
+            booksContainer.add(line, 0, 2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setReservedBooks(ArrayList<BookInstance> bookInstances){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(AuthorController.class.getResource("/z11/libraryapp/fxml/UserBooksReserved.fxml"));
+            VBox userBooksBox = fxmlLoader.load();
+            UserBooksReservedController userBooksController = fxmlLoader.getController();
+            userBooksController.setData(bookInstances, user_object, true);
+            booksContainer.add(userBooksBox, 0, 3);
+            Line line = new Line(-100, 0, 1105, 0);
+            line.setOpacity(0.12);
+            booksContainer.add(line, 0, 4);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setHistoryBooks(ArrayList<HistoryNode> historyNodes){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(AuthorController.class.getResource("/z11/libraryapp/fxml/UserBooks.fxml"));
+            VBox userBooksBox = fxmlLoader.load();
+            UserBooksController userBooksController = fxmlLoader.getController();
+            userBooksController.setData(historyNodes, user_object, "History", true);
+            booksContainer.add(userBooksBox, 0, 5);
+            Line line = new Line(-100, 0, 1105, 0);
+            line.setOpacity(0.12);
+            booksContainer.add(line, 0, 6);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void setData(User user){
         user_object = user;
         usernameLabel.setText(user_object.getLogin());
+        try {
+            DbHandler dbManager = new DbHandler();
+            ArrayList<Book> allBooks = dbManager.getBooks();
+            ArrayList<HistoryNode> borrowedBooks = new ArrayList<HistoryNode>();
+            ArrayList<BookInstance> reservedBooks = new ArrayList<BookInstance>();
+            ArrayList<HistoryNode> historyBooks = new ArrayList<HistoryNode>();
+            ArrayList<HistoryNode> historyNodes = dbManager.getHistoryNodes();
+            for (HistoryNode historyNode : historyNodes){
+                if (historyNode.getUserId() == user.getId() && historyNode.getDateReturned() == null){
+                    borrowedBooks.add(historyNode);
+                }
+                else if (historyNode.getUserId() == user.getId() && historyNode.getDateReturned() != null){
+                    historyBooks.add(historyNode);
+                }
+            }
+            for (Book book : allBooks){
+                ArrayList<BookInstance> bookInstances = dbManager.getBookInstances(book.getId());
+                for (BookInstance bookInstance : bookInstances){
+                    if (bookInstance.getUser_id() == user.getId() && bookInstance.getStatus().equals("RESERVED")){
+                        reservedBooks.add(bookInstance);
+                    }
+                }
+            }
+
+            setReadingBooks(borrowedBooks);
+            setReservedBooks(reservedBooks);
+            setHistoryBooks(historyBooks);
+        } catch (UnavailableDB e) {
+            e.printStackTrace();
+        }
     }
 
     private void search(Object event, String query) throws IOException, UnavailableDB{
